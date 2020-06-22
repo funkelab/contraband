41e59fd4546b718a29cfa6d058cb226cbdbb019a
riels89 <rileywhite@MacBook-Pro.local>
2020-06-22 12:23:59 -0400

    Update models dir to match current naming convention, and remove combination and log files

---
+++ models/Unet2D/EXP1-first_runs-6-3-20/Unet2D.py
@@ -0,0 +1,29 @@
+import torch
+import funlib
+from contraband.models.ContrastiveVolumeNet import (
+    SegmentationVolumeNet,
+    ContrastiveVolumeNet)
+from contraband.utils import load_model
+
+
+class Unet2D(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.name = "Unet2D"
+        self.pipeline = "Standard2D"
+
+        self.unet = funlib.learn.torch.models.UNet(
+            in_channels=1,
+            num_fmaps=12,
+            fmap_inc_factors=6,
+            downsample_factors=[(2, 2), (2, 2), (2, 2)],
+            kernel_size_down=[[(3, 3), (3, 3)]] * 4,
+            kernel_size_up=[[(3, 3), (3, 3)]] * 3,
+            constant_upsample=True)
+
+        self.out_channels = self.unet.out_channels
+        self.dims = self.unet.dims
+
+    def forward(self, raw):
+        return self.unet.forward(raw)
