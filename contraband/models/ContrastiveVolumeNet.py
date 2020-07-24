Fix normalization, numerical stability in loss.
The dim param in normalization was changed  because this apreaded to be what was working,
on closer analysis this was incorrect. The pameter that has been specified (and recently changed from 1 to 2) was actually the p var in norm. So the correct dim was specified (1)
and was given to the correct parameter.

The other issue that appreared at lower temperature params, was numerical instabilty
that was partly caused by just subtracting exp(1 / t) from the sumed simularities.
Instead the diagonal is not counted in the summation fixing this issue.

The loss was also changed to be a proper torch Module to precomputer the diag matrix for
the summation change.

contraband/models/ContrastiveVolumeNet.py
@@ -28,11 +28,11 @@ class ContrastiveVolumeNet(torch.nn.Module):
         # (b, c, dim_1, ..., dim_d)
         h_0 = self.base_encoder(raw_0)
         z_0 = self.projection_head(h_0)
-        z_0_norm = torch.nn.functional.normalize(z_0, 2)
+        z_0_norm = torch.nn.functional.normalize(z_0, dim=1)
 
         h_1 = self.base_encoder(raw_1)
         z_1 = self.projection_head(h_1)
-        z_1_norm = torch.nn.functional.normalize(z_1, 2)
+        z_1_norm = torch.nn.functional.normalize(z_1, dim=1)
 
         return h_0, h_1, z_0_norm, z_1_norm
 

contraband/pipelines/Contrastive.py
@@ -15,7 +15,7 @@ from contraband.pipelines.utils import (
     RandomPointGenerator,
     RandomSourceGenerator,
     RandomMultiBranchSource)
-from contraband.pipelines.contrastive_loss import contrastive_volume_loss
+from contraband.pipelines.contrastive_loss import ContrastiveVolumeLoss
 import daisy
 import numpy as np
 
@@ -31,15 +31,6 @@ class Contrastive():
         self.params = params
         self.logdir = logdir
         self.log_every = log_every
-
-        def loss(emb_0, emb_1, locations_0, locations_1):
-            return contrastive_volume_loss(
-                emb_0,
-                emb_1,
-                locations_0,
-                locations_1,
-                self.params['temperature'])
-        self.training_loss = loss
         self.val_loss = torch.nn.MSELoss()
 
     def _make_train_augmentation_pipeline(self, raw, source):
@@ -84,6 +75,11 @@ class Contrastive():
         is_2d = in_shape.dims() == 2
 
         emb_voxel_size = voxel_size
+
+        cv_loss = ContrastiveVolumeLoss(self.params['temperature'],
+                                        self.params['point_density'],
+                                        out_shape * voxel_size)
+
         # Add fake 3rd dim 
         if is_2d: 
             in_shape = gp.Coordinate((1, *in_shape))
@@ -99,6 +95,7 @@ class Contrastive():
         logger.info(f"in_shape: {in_shape}")
         logger.info(f"out_shape: {out_shape}")
         logger.info(f"voxel_size: {voxel_size}")
+
 
         request = gp.BatchRequest()
         request.add(raw_0, in_shape)
@@ -198,7 +195,7 @@ class Contrastive():
             pipeline + 
             gp.PreCache() +
             gp.torch.Train(
-                model, self.training_loss, optimizer,
+                model, cv_loss, optimizer,
                 inputs={
                     'raw_0': raw_0,
                     'raw_1': raw_1

contraband/pipelines/contrastive_loss.py
@@ -1,114 +1,117 @@
 import torch
 import numpy as np
 
-def contrastive_volume_loss(emb_0, emb_1, locations_0, locations_1, t):
-    '''Computes a contrastive learning loss for positive and negative pairs of
-    points in two volumes a and b.
-    This loss is known as NT-Xent for image classification [0].
-    Here, the same loss is applied to pairs of points within the same volume,
-    which is assumed to have been augmented in two different ways to yield
-    embedding volumes ``emb_0`` and ``emb_1``.
-    For the expected shapes, we use: b=batch, c=channels, d=spatial dimensions,
-    and n=number of points.
-    Args:
-        emb_0, emb_1 (``torch.Tensor``):
-            The embeddings extracted for two differently augmented versions of
-            the same volume. Embeddings are assumed to be normalized already
-            (i.e., magnitude is 1).
-            Expected shape: ``(b, c, dim_1, ..., dim_d)``.
-        locations_0, locations_1 (``torch.Tensor``):
-            List of integer coordinates to be used as indices into ``a`` and
-            ``b`, such that ``locations_0[i]`` is the same location as
-            ``locations_1[i]`` in the original volume.
-            Expected shape: ``(b, n, d)``.
-        t (``float``):
-            Temperature to be used in the NT-Xent loss.
-    [0] http://arxiv.org/abs/2002.05709
-    '''
+class ContrastiveVolumeLoss(torch.nn.Module):
+    def __init__(self, t, point_density, point_roi):
+        super().__init__()
+        self.t = t
 
-    n = locations_0.shape[1]
-    assert n == locations_1.shape[1], (
-        f"Different number of points given in locations_0 ({n}) and "
-        f"locations_1 ({locations_1.shape[1]})")
+        diag_size = int(2 * 2 * np.prod(point_roi) * point_density)
+        diag = np.eye(diag_size, diag_size)[np.newaxis]
+        self.non_diag_mask = torch.from_numpy((1 - diag).astype(np.bool))
 
-    # We expect the following shapes
-    # (b=batch, c=channels, d=spatial dimensions, n=num points)
+    def forward(self, emb_0, emb_1, locations_0, locations_1):
+        '''Computes a contrastive learning loss for positive and negative pairs of
+        points in two volumes a and b.
+        This loss is known as NT-Xent for image classification [0].
+        Here, the same loss is applied to pairs of points within the same volume,
+        which is assumed to have been augmented in two different ways to yield
+        embedding volumes ``emb_0`` and ``emb_1``.
+        For the expected shapes, we use: b=batch, c=channels, d=spatial dimensions,
+        and n=number of points.
+        Args:
+            emb_0, emb_1 (``torch.Tensor``):
+                The embeddings extracted for two differently augmented versions of
+                the same volume. Embeddings are assumed to be normalized already
+                (i.e., magnitude is 1).
+                Expected shape: ``(b, c, dim_1, ..., dim_d)``.
+            locations_0, locations_1 (``torch.Tensor``):
+                List of integer coordinates to be used as indices into ``a`` and
+                ``b`, such that ``locations_0[i]`` is the same location as
+                ``locations_1[i]`` in the original volume.
+                Expected shape: ``(b, n, d)``.
+            t (``float``):
+                Temperature to be used in the NT-Xent loss.
+        [0] http://arxiv.org/abs/2002.05709
+        '''
 
-    # emb_0      : (b, c, dim_1, ..., dim_d)
-    # emb_1      : (b, c, dim_1, ..., dim_d)
-    # locations_0: (b, n, d)
-    # locations_1: (b, n, d)
+        n = locations_0.shape[1]
+        assert n == locations_1.shape[1], (
+            f"Different number of points given in locations_0 ({n}) and "
+            f"locations_1 ({locations_1.shape[1]})")
 
-    assert emb_0.shape == emb_1.shape, \
-        "Embedding tensors do not have the same shape"
+        # We expect the following shapes
+        # (b=batch, c=channels, d=spatial dimensions, n=num points)
 
-    b, c, *volume_shape = emb_0.shape
-    d = len(volume_shape)
-    v = np.prod(volume_shape)
+        # emb_0      : (b, c, dim_1, ..., dim_d)
+        # emb_1      : (b, c, dim_1, ..., dim_d)
+        # locations_0: (b, n, d)
+        # locations_1: (b, n, d)
 
-    assert b == 1, "Batch size > 1 not yet implemented"
+        assert emb_0.shape == emb_1.shape, \
+            "Embedding tensors do not have the same shape"
 
-    # flatten the embeddings, spatial index first
-    # (b, v, c)
-    emb_0 = emb_0.view(b, c, v).transpose(2, 1)
-    emb_1 = emb_1.view(b, c, v).transpose(2, 1)
+        b, c, *volume_shape = emb_0.shape
+        d = len(volume_shape)
+        v = np.prod(volume_shape)
 
-    ind_kernel = torch.Tensor([
-        np.prod(volume_shape[i + 1:])
-        for i in range(d)
-    ]).float().to(locations_0.device)
+        assert b == 1, "Batch size > 1 not yet implemented"
 
-    # turn point coordinates into indices
-    # (n,) (TODO: concatenate batches here, do the same for embeddings)
-    ind_0 = torch.matmul(torch.floor(locations_0), ind_kernel).long().squeeze(dim=0)
-    ind_1 = torch.matmul(torch.floor(locations_1), ind_kernel).long().squeeze(dim=0)
+        # flatten the embeddings, spatial index first
+        # (b, v, c)
+        emb_0 = emb_0.view(b, c, v).transpose(2, 1)
+        emb_1 = emb_1.view(b, c, v).transpose(2, 1)
 
-    # get embeddings for each point
-    # (b, n, c)
-    emb_0 = emb_0.index_select(dim=1, index=ind_0)
-    emb_1 = emb_1.index_select(dim=1, index=ind_1)
+        ind_kernel = torch.Tensor([
+            np.prod(volume_shape[i + 1:])
+            for i in range(d)
+        ]).float().to(locations_0.device)
 
-    # concatenate embeddings of all points, such that a pos pair is indexed by
-    # i and i + n
-    # (b, 2n, c)
-    emb = torch.cat([emb_0, emb_1], dim=1)
+        # turn point coordinates into indices
+        # (n,) (TODO: concatenate batches here, do the same for embeddings)
+        ind_0 = torch.matmul(torch.floor(locations_0), ind_kernel).long().squeeze(dim=0)
+        ind_1 = torch.matmul(torch.floor(locations_1), ind_kernel).long().squeeze(dim=0)
 
-    # get all pairwise similarities sim_uv between all points (including within
-    # lists locations_0 and locations_1)
-    # (b, 2n, 2n)
-    sim = torch.matmul(emb, emb.transpose(2, 1))
+        # get embeddings for each point
+        # (b, n, c)
+        emb_0 = emb_0.index_select(dim=1, index=ind_0)
+        emb_1 = emb_1.index_select(dim=1, index=ind_1)
 
-    # precompute exp_uv = exp(s_uv/t)
-    # (b, 2n, 2n)
-    exp = torch.exp(sim/t)
+        # concatenate embeddings of all points, such that a pos pair is indexed by
+        # i and i + n
+        # (b, 2n, c)
+        emb = torch.cat([emb_0, emb_1], dim=1)
 
-    # get sum_u of all e_uw (with w≠u)
-    # (b, 2n)
-    sum_e = torch.sum(exp, dim=2) - np.exp(1.0/t)
+        # get all pairwise similarities sim_uv between all points (including within
+        # lists locations_0 and locations_1)
+        # (b, 2n, 2n)
+        sim = torch.matmul(emb, emb.transpose(2, 1))
 
-    # for each point u in either list
-    loss = 0
-    for u in range(n):
+        # precompute exp_uv = exp(s_uv/t)
+        # (b, 2n, 2n)
+        exp = torch.exp(sim / self.t)
 
-        # print(f"computing loss for point {u} and {u + n}")
+        # fill diagonal with zeros to avoid summing them.
+        mask = self.non_diag_mask[:, :2 * n, :2 * n].to(locations_0.device)
+        # get sum_u of all e_uw (with w≠u)
+        # (b, 2n)
+        sum_e = torch.sum(exp[mask].view((b, 2 * n, 2 * n - 1)), dim=2)
+        # for each point u in either list
+        loss = 0
+        for u in range(n):
 
-        # find corresponding point v in other list
-        v = u + n
-        # get e_uv (= e_vu)
-        e_uv = exp[:, u, v]
-        # l_uv = -log(e_uv/sum_u) - log(e_vu/sum_v)
-        loss_uv = -torch.sum(
-            torch.log(e_uv/sum_e[:, u]) +
-            torch.log(e_uv/sum_e[:, v]))
+            # print(f"computing loss for point {u} and {u + n}")
 
-        # print(f"sim_uv = {sim[:, u, v]}")
-        # print(f"e_uv = {e_uv}")
-        # print(f"sum_e[:, u] = {sum_e[:, u]}")
-        # print(f"sum_e[:, v] = {sum_e[:, v]}")
-        # print(f"loss_uv = {loss_uv}")
+            # find corresponding point v in other list
+            v = u + n
+            # get e_uv (= e_vu)
+            e_uv = exp[:, u, v]
+            # l_uv = -log(e_uv/sum_u) - log(e_vu/sum_v)
+            loss_uv = -torch.sum(
+                torch.log(e_uv/sum_e[:, u]) +
+                torch.log(e_uv/sum_e[:, v]))
 
-        loss += loss_uv
-
-    loss /= 2*n
-
-    return loss
+            loss += loss_uv
+        loss /= 2*n
+        
+        return loss

contraband/pipelines/test_loss.py
@@ -1,6 +1,8 @@
-from model import contrastive_volume_loss
+from contraband.pipelines.contrastive_loss import ContrastiveVolumeLoss
 import torch
 from torch.nn.functional import normalize
+
+
 def one_point_same():
     emb_0 = normalize(torch.tensor([[[[[-1,1],[1,1]],[[1,1],[1,1]]]]], dtype=torch.float32), dim=1)
     emb_1 = normalize(torch.tensor([[[[[-1,1], [1,1]],[[1,1],[1,1]]]]], dtype=torch.float32), dim=1)
@@ -12,11 +14,10 @@ def one_point_same():
     print(emb_1.size())
     print(locations_0.size())
     print(locations_1.size())
-
-    actual = contrastive_volume_loss(emb_0, emb_1, locations_0, locations_1, 1).numpy()
+    loss = ContrastiveVolumeLoss(t=1, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
     expected = 0.0
 
-    print("Pass: " + str(actual == expected))
     print("     Actual: " + str(actual) + " Expected: " + str(expected))
 
 def two_point_diff():
@@ -31,7 +32,8 @@ def two_point_diff():
     print(locations_0.size())
     print(locations_1.size())
 
-    actual = contrastive_volume_loss(emb_0, emb_1, locations_0, locations_1, 1).numpy()
+    loss = ContrastiveVolumeLoss(t=1, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
     expected = 2.2395448
 
     print("Pass: " + str(actual == expected))
@@ -40,7 +42,8 @@ def two_point_diff():
 def one_point_diff():
     emb_0 = normalize(torch.tensor([[[[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
     emb_1 = normalize(torch.tensor([[[[-1,1],[1,1]]]], dtype=torch.float32), dim=1)
-
+    print(emb_0)
+    print(emb_1)
     locations_0 = torch.tensor([[[0, 0]]], dtype=torch.float32)
     locations_1 = torch.tensor([[[0, 0]]], dtype=torch.float32)
 
@@ -49,34 +52,79 @@ def one_point_diff():
     print(locations_0.size())
     print(locations_1.size())
 
-    actual = contrastive_volume_loss(emb_0, emb_1, locations_0, locations_1, 1).numpy()
+    loss = ContrastiveVolumeLoss(t=1, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
     expected = 0
 
-    print("Pass: " + str(actual == expected))
     print("     Actual: " + str(actual) + " Expected: " + str(expected))
 
 def multi_channel_diff():
     emb_0 = normalize(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
     emb_1 = normalize(torch.tensor([[[[-1,1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
-
+    print(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32))
+    print(emb_0.shape)
     locations_0 = torch.tensor([[[0, 0], [0,1]]], dtype=torch.float32)
     locations_1 = torch.tensor([[[0, 0], [0,1]]], dtype=torch.float32)
     
-    print(emb_0)
-    print(emb_1)
-    print(emb_0.size())
-    print(emb_1.size())
+    print("emb_0:", emb_0)
+    print("emb_1", emb_1)
+    print("emb_0_size", emb_0.size())
+    print("emb_1_size", emb_1.size())
     print(locations_0.size())
     print(locations_1.size())
 
-    actual = contrastive_volume_loss(emb_0, emb_1, locations_0, locations_1, 1).numpy()
-    expected = 2.2395448
+    loss = ContrastiveVolumeLoss(t=1, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
+    expected = 0.861 
 
-    print("Pass: " + str(actual == expected))
     print("     Actual: " + str(actual) + " Expected: " + str(expected))
 
 
+def multi_channel_diff_multi_point():
+    emb_0 = normalize(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
+    emb_1 = normalize(torch.tensor([[[[-1,1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
+    print(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32))
+    print(emb_0.shape)
+    locations_0 = torch.tensor([[[0, 0], [0,1], [1,1]]], dtype=torch.float32)
+    locations_1 = torch.tensor([[[0, 0], [0,1], [1,1]]], dtype=torch.float32)
+    
+    print("emb_0:", emb_0)
+    print("emb_1", emb_1)
+    print("emb_0_size", emb_0.size())
+    print("emb_1_size", emb_1.size())
+    print(locations_0.size())
+    print(locations_1.size())
+
+    loss = ContrastiveVolumeLoss(t=1, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
+    expected = 1.3742 
+
+    print("     Actual: " + str(actual) + " Expected: " + str(expected))
+
+
+def multi_channel_diff_multi_point_diff_temp():
+    emb_0 = normalize(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
+    emb_1 = normalize(torch.tensor([[[[-1,1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32), dim=1)
+    print(torch.tensor([[[[1,-1],[1,1]], [[1,-1],[1,1]]]], dtype=torch.float32))
+    print(emb_0.shape)
+    locations_0 = torch.tensor([[[0, 0], [0,1], [1,1]]], dtype=torch.float32)
+    locations_1 = torch.tensor([[[0, 0], [0,1], [1,1]]], dtype=torch.float32)
+    
+    print("emb_0:", emb_0)
+    print("emb_1", emb_1)
+    print("emb_0_size", emb_0.size())
+    print("emb_1_size", emb_1.size())
+    print(locations_0.size())
+    print(locations_1.size())
+
+    loss = ContrastiveVolumeLoss(t=0.05, point_density=0.5, point_roi=(10,10))
+    actual = loss(emb_0, emb_1, locations_0, locations_1).numpy()
+    expected = 4.2575 
+
+    print("     Actual: " + str(actual) + " Expected: " + str(expected))
 one_point_same()
 one_point_diff()
 two_point_diff()
 multi_channel_diff()
+multi_channel_diff_multi_point()
+multi_channel_diff_multi_point_diff_temp()